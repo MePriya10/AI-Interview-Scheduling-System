66b629690bb4e7fb0359e090987cfebdbbeed23a
Priya Bisht <155985113+MePriya10@users.noreply.github.com>
2025-05-18 19:17:13 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,46 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const cors = require('cors');
-require('dotenv').config();
+// const authRoutes = require('./routes/authRoutes');
 
-const interviewRoutes = require('./routes/interviewRoutes');
+dotenv.config();
 
 const app = express();
+
+
+
+// MongoDB connection
 const PORT = process.env.PORT || 5000;
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/scheduleai';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Connected');
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})
+.catch(err => {
+  console.error('MongoDB connection error:', err.message);
+  process.exit(1); // exit process if DB fails to connect
+});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', require('./routes/authRoutes'));
+
+
+app.post('/check',(req,res)=>{
+  console.log('reached in backend');
+})
 
 // Routes
-app.use('/api/interviews', interviewRoutes);
+//app.use('/api/auth', authRoutes); // POST /api/auth/register or /api/auth/login
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('MongoDB Connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch((err) => console.error('MongoDB connection error:', err));
+

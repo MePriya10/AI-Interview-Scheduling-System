3b7396fc630eead6660d48b451beb1bba99b8949
Priya Bisht <155985113+MePriya10@users.noreply.github.com>
2025-05-19 15:15:43 +0530

    updated backend

---
+++ Backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  role: { type: String, enum: ["admin", "interviewer"], default: "interviewer" },
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true, minlength: 6 },
+  
+}, { timestamps: true });
+
+
+ module.exports = mongoose.model('User', userSchema);
+
+
---
+++ Backend/routes/scheduleRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { scheduleInterview, getInterviews } = require("../controllers/interviewController");
+// You can later import your controller here
+// const { scheduleInterview, getInterviews } = require("../controllers/interviewController");
+
+// Dummy GET route (just to test that it's working)
+router.get("/", (req, res) => {
+  res.send("Schedule route is working!");
+});
+
+module.exports = router;
